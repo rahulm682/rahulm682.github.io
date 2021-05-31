a942007cf9b1d22b972a9a14bdaad61f1ce24f70
rahulm682 <66553483+rahulm682@users.noreply.github.com>
2021-05-31 10:11:51 +0530

    Add files via upload

--- vaccine.js
+++ vaccine.js
@@ -1,15 +1,47 @@
 let stateObj, districtObj;
 
+// State Dropdown and District Dropdown
 let stateSelection = document.getElementById("stateSelector");
 let districtSelection = document.getElementById("districtSelector");
-let dataDisplay = document.getElementById('data');
-let submit = document.getElementById('submit');
+// let dataDisplay = document.getElementById('data');
 
+// Respective Buttons for fetching results
+let submitDistrict = document.getElementById('submitDistrict');
+let submitPincode = document.getElementById('submitPincode');
+
+// The tabs 
+let districtFinder = document.getElementById('district');
+let pincodeFinder = document.getElementById('pincode');
 // console.log(stateSelection);
 
-stateFetcher();
+// Error section
+let noData = document.getElementById('noData');
+
 stateSelection.addEventListener('change', districtFetcher);
-submit.addEventListener('click', getDetails);
+submitDistrict.addEventListener('click', getDetails);
+submitPincode.addEventListener('click', getResult);
+
+
+stateFetcher();
+
+districtFinder.addEventListener('click', ()=>{
+    districtFinder.classList.add('active');
+    pincodeFinder.classList.remove('active');
+    let d = document.getElementById('getByDistrict');
+    let p = document.getElementById('getByPincode');
+    p.classList.add('hide');
+    d.classList.remove('hide');
+});
+
+pincodeFinder.addEventListener('click', ()=>{
+    pincodeFinder.classList.add('active');
+    districtFinder.classList.remove('active');
+    let d = document.getElementById('getByDistrict');
+    let p = document.getElementById('getByPincode');
+    d.classList.add('hide');
+    p.classList.remove('hide');
+});
+
 
 async function stateFetcher() {
     let stateUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
@@ -24,7 +56,7 @@ async function stateFetcher() {
             stateSelection.add(option);
         });
     } else {
-        console.log("Error" + response.status);
+        noData.innerHTML = "Some Error occured. Error code: " + response.status;
     }
 } 
 
@@ -45,20 +77,21 @@ async function districtFetcher() {
             districtSelection.add(option);
         });
     } else {
-        console.log("Error" + response.status);
+        noData.innerHTML = "Some Error occured. Error code: " + response.status;
     }
 } 
 
 async function getDetails() {
-    let d1 = document.getElementById('dateSelector').value;
+    let result = document.getElementById('resultDistrict');
+    let d1 = document.getElementById('dateSelectorDistrict').value;
     let date = new Date(d1);
-    console.log(date);
-    let id = districtSelection.options[districtSelection.selectedIndex].value;
-
     let dateStr = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
-    console.log(dateStr);
+    // console.log(date);
+    // console.log(dateStr);
+
+    let id = districtSelection.options[districtSelection.selectedIndex].value;
     let detailsUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${dateStr}`;
-    console.log(detailsUrl);
+    // console.log(detailsUrl);
 
     let response = await fetch(detailsUrl);
 
@@ -66,21 +99,60 @@ async function getDetails() {
         let data = await response.json();
         // if(data.size>=0) 
         if(data.sessions.length>0) {
-            card(data);
+            card(data, result);
         } else {
             setTimeout(()=>{
                 result.innerHTML = "No Slots Found!!!!!";
             }, 0);
         }
-        console.log(data);
+        // console.log(data);
     } else {
-        console.log("Error" + response.status);
+        noData.innerHTML = "Some Error occured. Error code: " + response.status;
     }
 }
 
 
-function card(obj) {
-    let result = document.getElementById('result');
+
+let pincodeInput = document.getElementById('pincodeInput');
+pincodeInput.addEventListener('input', (e)=> {
+    if(pincodeInput.value.length>6) {
+        let pinErr = document.getElementById('pinErr');
+        pinErr.innerHTML = "Please enter valid pincode of six numbers";
+        pinErr.style = 'color: red';
+    } else {
+        pinErr.innerHTML = "";
+    }
+});
+
+
+async function getResult() {
+    let result = document.getElementById('resultPincode');
+    let pincode = document.getElementById('pincodeInput').value;
+    let d1 = document.getElementById('dateSelectorPincode').value;
+    let date = new Date(d1);
+    let dateStr = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+    
+    let pincodeUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${dateStr}`;
+    
+    let response = await fetch(pincodeUrl);
+    
+    if(response.status == 200) {
+        let data = await response.json();
+        if(data.sessions.length>0) {
+            card(data, result);
+        } else {
+            setTimeout(()=>{
+                result.innerHTML = "No Slots Found!!!!!";
+            }, 0);
+        }
+    } else {
+        noData.innerHTML = "Some Error occured. Error code: " + response.status;
+    }
+}
+
+
+function card(obj, result) {
+    // let result = document.getElementById('result');
     setTimeout(()=>{
         result.innerHTML = "";
     }, 0);
@@ -114,6 +186,3 @@ function card(obj) {
         }, 0);
     });
 }
-
-
-                
